422bf40bb92158eb33b74dc53808412750b5e609
gizawsim <gizawsim@gmail.com>
2022-11-15 20:28:07 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Starts Flask web app
-Listening on 0.0.0.0:5000
-Route '/' displays "Hello HBNB!"
-"""
+'''A simple Flask web application.
+'''
 from flask import Flask
 
+
 app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_route():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+@app.route('/')
+def index():
+    '''The home page.'''
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
